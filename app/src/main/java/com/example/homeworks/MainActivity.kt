db6ee614b0393e05bea766c81cd39874535aff69
Sergey <seregaryz@gmail.com>
2019-12-12 12:20:46 +0300

    Revert "fix my eighth homework(version 1.0)"
    This reverts commit decf169c

--- app/src/main/java/com/example/homeworks/MainActivity.kt
+++ app/src/main/java/com/example/homeworks/MainActivity.kt
@@ -2,35 +2,11 @@ package com.example.homeworks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.homeworks.music.MusicAdapter
-import com.example.homeworks.music.MusicDAO.Companion.musicList
-import com.example.homeworks.music.Song
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var adapter: MusicAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        adapter = MusicAdapter(musicList) { music ->
-            navigateToPlayer(music)
-        }
-
-        rv_music.adapter = adapter
     }
-
-
-    private fun navigateToPlayer(
-        song: Song
-    ) {
-        MusicService.startService(this, musicList.indexOf(song), song)
-        startActivity(PlayerActivity.createIntent(this, song))
-    }
-
-
-
-
 }
--- app/src/main/java/com/example/homeworks/music/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.example.homeworks.music
-
-data class Song(
-    var title: String,
-    var singer: String,
-    var album: String,
-    var poster: Int,
-    var music: Int
-    )
